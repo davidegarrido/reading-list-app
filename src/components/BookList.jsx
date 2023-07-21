bdf0dfb97e9a492094648f88111e50d0ac20e635
davide.g <davide.garrido@fastcomcorp.cl>
2023-07-21 09:56:02 -0400

    readingListSet

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,15 +1,16 @@
 import { Card } from "./Card";
 export const BookList = ({ books, readingList }) => {
-  const isBookInReadingList = (ISBN) => {
-    return readingList.some((b) => b.book.ISBN === ISBN);
-  };
+  // const isBookInReadingList = (ISBN) => {
+  //   return readingList.some((b) => b.book.ISBN === ISBN);
+  // };
+  const readingListSet = new Set(readingList.map((b) => b.book.ISBN));
 
   return (
     <div className="mb-10 grid gap-5 grid-cols-1 xl:grid-cols-4">
       {books.map(({ book }) => (
         <Card
           key={book.ISBN}
-          inReadingList={isBookInReadingList(book.ISBN)}
+          inReadingList={readingListSet.has(book.ISBN)}
           title={book.title}
           pages={book.pages}
           genre={book.genre}
